48f68e0e8cb43d86986ece44a900a35ed9241d2c
Yousuf <75563957+mohammedyousuf051@users.noreply.github.com>
2022-09-14 13:23:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
-// var bodyParser = require('body-parser')
-
-// app.use(bodyParser.urlencoded({ extended: false }))
-
-
 
 app.get('/9daysweather', async(req,res) =>{
     const URL = 'https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en';
     const fetchresp = await fetch(URL);
     const json = await fetchresp.json()
-    const dataa = async (arr) => {
+    const Wdata = async (arr) => {
         
         for(var i in json.weatherForecast){
             var weatherData = json.weatherForecast[i];
@@ -18,7 +13,7 @@ app.get('/9daysweather', async(req,res) =>{
         }
         return arr
     };
-    const data = await dataa([])
+    const data = await Wdata([])
     res.json(data);
 });
 app.listen(3000);
